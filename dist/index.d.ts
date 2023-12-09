04cfd769785edf8f8b015d1c9124147ab417c8ff
wuguishifu <49931047+wuguishifu@users.noreply.github.com>
2023-12-09 14:17:20 -0800

    removed dist files

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-type ComposerProps = {
-    components: Array<React.JSXElementConstructor<unknown & {
-        children: React.ReactNode;
-    }>>;
-    children: React.ReactNode;
-};
-declare const _default: ({ components, children }: ComposerProps) => React.JSX.Element;
-export default _default;
